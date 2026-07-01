f777b3aba7c9dd42658f656dcea0912f2e5f01e2
Angel <angel@stokes-visualizer.dev>
2026-07-01 00:33:24 -0400

    feat: add Store base class

---
+++ src/stores/Store.ts
@@ -0,0 +1,29 @@
+type Listener<T> = (state: T, prev: T) => void;
+
+export class Store<T extends Record<string, unknown>> {
+  private state: T;
+  private listeners: Set<Listener<T>> = new Set();
+
+  constructor(initial: T) {
+    this.state = initial;
+  }
+
+  getState(): Readonly<T> {
+    return this.state;
+  }
+
+  setState(partial: Partial<T>): void {
+    const prev = this.state;
+    this.state = { ...this.state, ...partial };
+    for (const fn of this.listeners) {
+      fn(this.state, prev);
+    }
+  }
+
+  subscribe(fn: Listener<T>): () => void {
+    this.listeners.add(fn);
+    return () => {
+      this.listeners.delete(fn);
+    };
+  }
+}
